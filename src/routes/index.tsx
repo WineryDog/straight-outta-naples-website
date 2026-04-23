3b6254ad7572ffcf515ded60265e809acc28379f
WineryDog <frank8rsl@gmail.com>
2026-04-23 23:17:11 +0200

    Add Catering & MenuComingSoon

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,19 +1,22 @@
 import { createBrowserRouter } from 'react-router'
 import RootLayout from '../pages/RootLayout'
 import HomePage from '../pages/HomePage'
+import Catering from '../pages/Catering'
 import ComingSoon from '../components/ComingSoon'
+import MenuComingSoon from '../components/MenuComingSoon'
 
 export const router = createBrowserRouter(
-  [ 
+  [
     {
       element: <RootLayout />,
       children: [
         { path: '/', element: <HomePage /> },
-        { path: '/menu', element: <ComingSoon /> },
+        { path: '/menu', element: <MenuComingSoon/> },
         { path: '/reservations', element: <ComingSoon /> },
         { path: '/functions', element: <ComingSoon /> },
         { path: '/order', element: <ComingSoon /> },
         { path: '/events', element: <ComingSoon /> },
+        { path: '/catering', element: <Catering /> }
       ],
     }
   ],
