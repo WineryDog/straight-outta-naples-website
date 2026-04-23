d58d78c22f8d41dcf7162dbf7ee3a17935ae2132
WineryDog <frank8rsl@gmail.com>
2026-04-24 00:31:09 +0200

    Fix Routes Refresh & Scroll

--- src/pages/RootLayout.tsx
+++ src/pages/RootLayout.tsx
@@ -1,10 +1,20 @@
-import { Outlet } from 'react-router'
+import { useEffect } from 'react'
+import { Outlet, useLocation } from 'react-router'
 import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
+
+function ScrollToTop() {
+  const { pathname } = useLocation()
+  useEffect(() => {
+    window.scrollTo(0, 0)
+  }, [pathname])
+  return null
+}
 
 export default function RootLayout() {
   return (
     <>
+      <ScrollToTop />
       <Navbar />
       <main>
         <Outlet />
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,22 +1,32 @@
+import { lazy, Suspense } from 'react'
 import { createBrowserRouter } from 'react-router'
 import RootLayout from '../pages/RootLayout'
-import HomePage from '../pages/HomePage'
-import Catering from '../pages/Catering'
-import ComingSoon from '../components/ComingSoon'
-import MenuComingSoon from '../components/MenuComingSoon'
+
+const HomePage       = lazy(() => import('../pages/HomePage'))
+const Catering       = lazy(() => import('../pages/Catering'))
+const MenuComingSoon = lazy(() => import('../components/MenuComingSoon'))
+const ComingSoon     = lazy(() => import('../components/ComingSoon'))
+
+function Page({ component: Component }: { component: React.ComponentType }) {
+  return (
+    <Suspense fallback={<div className="min-h-[60vh] bg-crema" />}>
+      <Component />
+    </Suspense>
+  )
+}
 
 export const router = createBrowserRouter(
   [
     {
       element: <RootLayout />,
       children: [
-        { path: '/', element: <HomePage /> },
-        { path: '/menu', element: <MenuComingSoon/> },
-        { path: '/reservations', element: <ComingSoon /> },
-        { path: '/functions', element: <ComingSoon /> },
-        { path: '/order', element: <ComingSoon /> },
-        { path: '/events', element: <ComingSoon /> },
-        { path: '/catering', element: <Catering /> }
+        { path: '/',             element: <Page component={HomePage} /> },
+        { path: '/menu',         element: <Page component={MenuComingSoon} /> },
+        { path: '/reservations', element: <Page component={ComingSoon} /> },
+        { path: '/functions',    element: <Page component={ComingSoon} /> },
+        { path: '/order',        element: <Page component={ComingSoon} /> },
+        { path: '/events',       element: <Page component={ComingSoon} /> },
+        { path: '/catering',     element: <Page component={Catering} /> },
       ],
     }
   ],
