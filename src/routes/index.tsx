b89860910b77b48feeae751477913528374ce3eb
WineryDog <frank8rsl@gmail.com>
2026-04-23 15:24:05 +0200

    Fix Basename in Router

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,16 +3,21 @@ import RootLayout from '../pages/RootLayout'
 import HomePage from '../pages/HomePage'
 import ComingSoon from '../components/ComingSoon'
 
-export const router = createBrowserRouter([
+export const router = createBrowserRouter(
+  [ 
+    {
+      element: <RootLayout />,
+      children: [
+        { path: '/', element: <HomePage /> },
+        { path: '/menu', element: <ComingSoon /> },
+        { path: '/reservations', element: <ComingSoon /> },
+        { path: '/functions', element: <ComingSoon /> },
+        { path: '/order', element: <ComingSoon /> },
+        { path: '/events', element: <ComingSoon /> },
+      ],
+    }
+  ],
   {
-    element: <RootLayout />,
-    children: [
-      { path: '/', element: <HomePage /> },
-      { path: '/menu', element: <ComingSoon /> },
-      { path: '/reservations', element: <ComingSoon /> },
-      { path: '/functions', element: <ComingSoon /> },
-      { path: '/order', element: <ComingSoon /> },
-      { path: '/events', element: <ComingSoon /> },
-    ],
-  },
-])
+    basename: import.meta.env.BASE_URL,
+  }
+);
